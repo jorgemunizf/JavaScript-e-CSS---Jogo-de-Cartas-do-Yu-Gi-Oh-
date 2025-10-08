63b90ac97a78392a7bc798dc7dfa54eca10a32e4
Jorge Filho <jorge.munizfilho@gmail.com>
2025-10-07 21:25:08 -0300

    Criado os estados dos elementos

---
+++ src/scripts/engine.js
@@ -0,0 +1,23 @@
+const state = {
+  score: {
+    playerScore: 0,
+    computarScore: 0,
+    scoreBox: document.getElementById("score_points"),
+  },
+  cardSprites: {
+    avatar: document.getElementById("card-image"),
+    name: document.getElementById("card-name"),
+    type: document.getElementById("card-type"),
+  },
+  fieldsCards: {
+    palyer: document.getElementById("player-field-card"),
+    computer: document.getElementById("computer-field-card"),
+  },
+  actions: {
+    button: document.getElementById("next-duel"),
+  },
+};
+
+function init() {}
+
+init();
